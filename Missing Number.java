47a41bbe5b6e11bb240a3773c708d55fe0a3c2cc
Swathi Ramesh <157286054+swathiramesh24@users.noreply.github.com>
2025-12-18 15:13:08 +0530

    Add Missing Number problem solution
    Implement a solution to find the missing number in an array.

---
+++ Missing Number.java
@@ -0,0 +1,24 @@
+/*
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+Example 1:
+Input: nums = [3,0,1]
+Output: 2
+Explanation:
+n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+*/
+
+//code
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=i)
+            {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
